c56770589a872342410daf6374b2820ccad6ad0a
Mayuresh Pingale <mayureshpingale1156@gmail.com>
2024-07-02 18:48:33 -0500

    Exit Command

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace::std;
+
+vector<string> extractTokens(string s);
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,8 +15,29 @@ int main() {
       cout << "$ ";
       string input;
       getline(std::cin, input);
-  
-      cout << input << commandNotFoundError;
+      vector<string> tokens = extractTokens(input);
+
+      if(tokens[0].compare("exit") == 0) {
+        exit(0);
+      }
+      else{
+        cout << input << commandNotFoundError;
+      }
+      
       cout<<"\n";
   }
+}
+
+vector<string> extractTokens(string s) {
+    vector<string> tokens;
+    string del = " ";
+
+    int start, end = -1*del.size();
+    do {
+        start = end + del.size();
+        end = s.find(del, start);
+        tokens.push_back(s.substr(start, end - start));
+    } while (end != -1);
+    
+    return tokens;
 }
