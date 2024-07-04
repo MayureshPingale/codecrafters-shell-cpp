95e0fdf7618fcd9991f72282ff72b35d0df0fe22
Mayuresh Pingale <mayureshpingale1156@gmail.com>
2024-07-03 19:29:54 -0500

    Type Command

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 using namespace::std;
 
 vector<string> extractTokens(string s);
+string exec(const char* cmd); 
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -17,11 +18,18 @@ int main() {
       getline(std::cin, input);
       vector<string> tokens = extractTokens(input);
 
+      //int resultCode  = system("which " + tokens )
+
       if(tokens[0].compare("exit") == 0) {
         exit(0);
       }
       else if(tokens[0].compare("echo") == 0) {
         cout << input.substr(5);
+      }
+      else if(tokens[0].compare("type") == 0) {
+        string tpCommand = "type "+ tokens[1]; 
+        string result = exec(tpCommand.c_str());
+        cout << result.substr(0, result.size() - 1); // Removing the extra "\n";
       }
       else{
         cout << input << commandNotFoundError;
@@ -43,4 +51,19 @@ vector<string> extractTokens(string s) {
     } while (end != -1);
     
     return tokens;
+}
+
+string exec(const char* cmd) {
+    array<char, 128> buffer;
+    string result;
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
+      result += buffer.data();
+    }
+
+
+    return result;
 }
