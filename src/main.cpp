c4954cca9d5eb5f2cdab67c6b196e6382903de30
Mayuresh Pingale <mayureshpingale1156@gmail.com>
2024-07-04 20:31:12 -0500

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include<bits/stdc++.h>
+#include <sstream>
+#include <filesystem>
 using namespace::std;
 
 vector<string> extractTokens(string s);
@@ -10,6 +12,8 @@ int main() {
   cout << std::unitbuf;
   cerr << std::unitbuf;
   string commandNotFoundError(": command not found");
+
+  set<string> validCommands({"echo", "cd", "exit"});
 
   // Uncomment this block to pass the first stage
   while (true){
@@ -27,9 +31,22 @@ int main() {
         cout << input.substr(5);
       }
       else if(tokens[0].compare("type") == 0) {
-        string tpCommand = "type "+ tokens[1]; 
-        string result = exec(tpCommand.c_str());
-        cout << result.substr(0, result.size() - 1); // Removing the extra "\n";
+        if(validCommands.find(tokens[1]) != validCommands.end()) {
+          cout<< tokens[1] <<" is a shell builtin";
+        }
+        else{
+          // string tpCommand = "type "+ tokens[1]; 
+          // string result = exec(tpCommand.c_str());
+          // cout << result.substr(0, result.size() - 1); // Removing the extra "\n";
+
+          std::string path = get_path(input);
+          if(path.empty()){
+              std::cout<<tokens[1]<<": not found\n";
+          }
+          else{
+              std::cout<<input<<" is "<<path<<std::endl;
+          }
+        }
       }
       else{
         cout << input << commandNotFoundError;
@@ -66,4 +83,18 @@ string exec(const char* cmd) {
 
 
     return result;
+}
+
+std::string get_path(std::string command){
+    std::string path_env = std::getenv("PATH");
+    std::stringstream ss(path_env);
+    std::string path;
+    while(!ss.eof()){
+        getline(ss, path, ':');
+        string abs_path = path + '/' + command;
+        if(filesystem::exists(abs_path)){
+            return abs_path;
+        }
+    }
+    return "";  
 }
